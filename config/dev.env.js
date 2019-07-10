a570d9258423944e33604800912f0d6c8e6cf994
shinanaa <916457399@qq.com>
2019-07-10 09:33:05 +0800

    请求地址更新

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,6 @@
 const useUrl = 'p'
-const mock_url = 'http://192.168.4.11:3000'
+// const mock_url = 'http://192.168.4.11:3000'
+const mock_url = 'https://www.easy-mock.com/mock/5cf477f520156326ddbeb251/example/'
 const url={
     l: 'http://192.168.4.12:8080/api/', //真实
     t: 'http://127.0.0.1:3000',  //测试
